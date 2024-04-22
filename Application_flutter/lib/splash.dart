1a36b69e9c45f94bca5c02181954b43aa06c3b53
Venkatnvs <97899253+Venkatnvs@users.noreply.github.com>
2024-04-23 02:26:15 +0530

    Add Flutter app and splash screen

---
+++ Application_flutter/lib/splash.dart
@@ -0,0 +1,50 @@
+  import 'package:flutter/material.dart';
+
+  class SplashScreen extends StatefulWidget {
+    final Duration duration;
+    final Widget navigateAfterDuration;
+
+    SplashScreen({
+      required this.duration,
+      required this.navigateAfterDuration,
+    });
+
+    @override
+    _SplashScreenState createState() => _SplashScreenState();
+  }
+
+  class _SplashScreenState extends State<SplashScreen> {
+    @override
+    void initState() {
+      super.initState();
+      _navigateToNextScreen();
+    }
+
+    _navigateToNextScreen() async {
+      await Future.delayed(widget.duration);
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => widget.navigateAfterDuration,
+        ),
+      );
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        body: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Positioned.fill(
+              child: Center(
+                child: Image.asset(
+                  'assets/logo.gif',
+                  width: 200,
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+  }
